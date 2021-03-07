1e80f87d004247c06eff95fe4d24465cb1ec0c04
Raghvendra Jain <raghavendra.jain@gmail.com>
2021-03-07 18:49:07 +0900

    age and gender webapp

--- app/server.py
+++ app/server.py
@@ -13,17 +13,22 @@ from starlette.staticfiles import StaticFiles
 
 from utils import *
 
-mask_detector_file_url = 'https://www.dropbox.com/s/xl90ovl3bwgg1ql/mask_detector-model.pkl?dl=1'
+mask_detector_file_url = 'https://www.dropbox.com/s/97ox65x1va6pj4w/mask_detector-model.pkl?dl=1'
 mask_detector_file_name = 'mask_detector-model.pkl'
 
 
-masked_gender_detector_file_url  = "https://www.dropbox.com/s/9velh2c5ql194mx/gender_mask-model.pkl?dl=1" 
+masked_gender_detector_file_url  = "https://www.dropbox.com/s/9l8ga1pup9e8jas/gender_mask-model.pkl?dl=1" 
 masked_gender_detector_file_name = "gender_mask-model.pkl"
 
 
-non_masked_gender_detector_file_url  = "https://www.dropbox.com/s/dhb7qgnrur26dsi/gender_no_mask-model.pkl?dl=1"
+non_masked_gender_detector_file_url  = "https://www.dropbox.com/s/tqydjvtwtsa7qrj/gender_no_mask-model.pkl?dl=0"
 non_masked_gender_detector_file_name = "gender_no_mask-model.pkl"
 
+masked_age_file_url = "https://www.dropbox.com/s/ghwjjn7s57ggt3h/age_classifier_mask-model.pkl?dl=1"
+masked_age_file_name = "age_classifier_mask-model.pkl"
+
+non_masked_age_file_url = "https://www.dropbox.com/s/pkpuhp9u1jhl8t0/age_classifier_no_mask-model.pkl?dl=0"
+non_masked_age_file_name = "age_classifier_no_mask-model.pkl"
 
 
 # classes = ['mask', 'no_mask']
@@ -86,6 +91,35 @@ async def setup_learner_nomaskgender():
             raise
 
 
+
+async def setup_learner_maskage():
+    await download_file(masked_age_file_url, path / masked_age_file_name)
+    try:
+        learn   = load_learner(path/masked_age_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+async def setup_learner_nomaskage():
+    await download_file(non_masked_age_file_url, path / non_masked_age_file_name)
+    try:
+        learn   = load_learner(path/non_masked_age_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
 res_prototxt = "deploy.prototxt.txt"
 res_model    = "res10_300x300_ssd_iter_140000.caffemodel"
 net = cv2.dnn.readNetFromCaffe(f"app/{res_prototxt}", f"app/{res_model}")
@@ -98,8 +132,13 @@ tasks_2 = [asyncio.ensure_future(setup_learner_maskgender())]
 mask_gender = loop.run_until_complete(asyncio.gather(*tasks_2))[0]
 
 tasks_3 = [asyncio.ensure_future(setup_learner_nomaskgender())]
-mask_nogender = loop.run_until_complete(asyncio.gather(*tasks_3))[0]
+nomask_gender = loop.run_until_complete(asyncio.gather(*tasks_3))[0]
 
+tasks_4 = [asyncio.ensure_future(setup_learner_maskage())]
+mask_age = loop.run_until_complete(asyncio.gather(*tasks_4))[0]
+
+tasks_5 = [asyncio.ensure_future(setup_learner_nomaskage())]
+nomask_age = loop.run_until_complete(asyncio.gather(*tasks_5))[0]
 
 
 loop.close()
@@ -125,11 +164,24 @@ async def analyze(request):
     # prediction = learn.predict(img)[0]
     is_masked = mask_detector.predict(processed_file)[0]
     if is_masked == 'True':
-        prediction = mask_gender.predict(processed_file)[0]
+        prediction_gender = mask_gender.predict(processed_file)[0]
+        prediction_age = mask_age.predict(processed_file)[0]
     elif is_masked == 'False':
-        prediction = mask_nogender.predict(processed_file)[0]
-    print(f"The prediction is {prediction}")
-    return JSONResponse({'result': str(prediction)})
+        prediction_gender = nomask_gender.predict(processed_file)[0]
+        prediction_age = nomask_age.predict(processed_file)[0]
+    if prediction_age == "0":
+        age = "4-12"
+    elif prediction_age == "1":
+        age = "13-19"
+    elif prediction_age == "2":
+        age = "20-34"
+    elif prediction_age == "3":
+        age = "35-49"
+    elif prediction_age == "4":
+        age = "50+"
+    print(f"The gender is {prediction_gender} and age is {age}")
+    result = f"The gender is {prediction_gender} and age is {age}"
+    return JSONResponse({'result': result})
 
 
 if __name__ == '__main__':
